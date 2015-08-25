c4dafa681111d7b90fa91b3cdb80a12a7071a858
毛林 <maolin@shanlitech.com>
2015-08-25 18:45:19 +0800

    implement RIPublisher as an ILoopable

--- include/ris/pubsub/publisher.h
+++ include/ris/pubsub/publisher.h
@@ -4,14 +4,17 @@
 #include <czmq.h>
 
 #include "ris/ritypes.h"
+#include "ris/loopable.h"
+#include "ris/riobserver.h"
+#include "ris/region/regiontable.h"
 
-class RIPublisher {
+class RIPublisher : public ILoopable, public IRIObserver {
 public:
-	RIPublisher();
+	RIPublisher(zloop_t* loop);
 	~RIPublisher();
 
-	int start(const std::list<std::string>& brokers);
-	int shutdown();
+	int start(const std::shared_ptr<RIRegionTable>& table, const std::list<std::string>& brokers);
+	int stop();
 
 
 	int pubRegion(const RegionRt& region);
@@ -22,11 +25,29 @@ public:
 
 	int pubPayload(const RegionRt& region,const Payload& pl);
 	int pubRemovePayload(const RegionRt& region,const uuid_t& pl);
-	
 private:
-	std::list<std::string>		m_brokers;
-	zsock_t*					m_pub;
+	virtual int startLoop(zloop_t* loop);
+	virtual void stopLoop(zloop_t* loop);
 
+	virtual void onNewRegion(const Region& reg) = 0;
+	virtual void onDelRegion(const uuid_t& reg) = 0;
+	virtual void onNewService(const RegionRt& reg,const Service& svc) = 0;
+	virtual void onDelService(const RegionRt& reg,const uuid_t& svc) = 0;
+	virtual void onNewPayload(const RegionRt& reg,const Payload& pl) = 0;
+	virtual void onDelPayload(const RegionRt& reg,const uuid_t& pl) = 0;
+private:
+	zsock_t* connectBrokers();
+
+	int pubRepeated();
+	static int onRegionPubTimer(zloop_t *loop, int timer_id, void *arg);
+	static int onRepeatPubTimer(zloop_t *loop, int timer_id, void *arg);
+private:
+	zloop_t*						m_loop;
+	zsock_t*						m_pub;
+	std::list<std::string>			m_brokers;
+	std::shared_ptr<RIRegionTable>	m_table;
+	int								m_tid_reg;
+	int								m_tid_repeat;
 private:
 	RIPublisher(const RIPublisher&) = delete;
 	RIPublisher(const RIPublisher&&) = delete;
--- src/pubsub/publisher.cxx
+++ src/pubsub/publisher.cxx
@@ -1,54 +1,120 @@
 #include "ris/pubsub/publisher.h"
 
 
-RIPublisher::RIPublisher() :
-	m_pub(nullptr) {
-	}
-
-
-RIPublisher::~RIPublisher() {
-	shutdown();
+RIPublisher::RIPublisher(zloop_t* loop) :
+	m_loop(loop),
+	m_pub(nullptr),
+	m_tid_reg(-1),
+	m_tid_repeat(-1)
+{
 }
 
 
-int RIPublisher::start(const std::list<std::string>& brokers) {
+RIPublisher::~RIPublisher() {
+	stop();
+}
+
+
+int RIPublisher::start(const std::shared_ptr<RIRegionTable>& table,const std::list<std::string>& brokers) {
 	int result = -1;
 	assert( ! brokers.empty() );
 	do {
 		if( nullptr != m_pub )
 			break;
-		m_pub = zsock_new(ZMQ_PUB);
-		if( nullptr == m_pub )
-			break;
 		m_brokers = brokers;
 
-		result = 0;
-		for( auto it = m_brokers.begin(); it != m_brokers.end(); ++it) {
-			if( -1 == zsock_connect(m_pub,"%s",it->c_str()) ) {
-				result = -1;
-				m_brokers.clear();
-				break;
-			}
-		}
+		m_pub = connectBrokers();
+		if( nullptr == m_pub )
+			break;
+		m_table = table;
+		m_table->setObserver(this);
+
+		pubRegion( m_table->region() );
+
+		result = startLoop(m_loop);
 	} while( 0 );
 
 	if( result == -1 ) {
+		stopLoop(m_loop);
 		if( m_pub ) {
 			zsock_destroy(&m_pub);
 		}
+		if( m_table ) {
+			m_table->unsetObserver();
+			m_table = nullptr;
+		}
+		m_brokers.clear();
 	}
 
 	return result;
 }
 
-int RIPublisher::shutdown() {
-	if( m_pub ) {
-		zsock_destroy(&m_pub);
-		m_brokers.clear();
-		return 0;
-	} else {
+int RIPublisher::stop() {
+	if( nullptr == m_pub ) {
 		return -1;
 	}
+
+	stopLoop(m_loop);
+	if( m_table ) {
+		pubRemoveRegion( m_table->region().id );
+	}
+
+	if( m_table ) {
+		m_table->unsetObserver();
+		m_table = nullptr;
+	}
+	m_brokers.clear();
+
+	if( m_pub ) {
+		zsock_destroy(&m_pub);
+	}
+	return 0;
+}
+
+int RIPublisher::startLoop(zloop_t* loop) {
+	m_tid_reg = zloop_timer(loop,5000,0,onRegionPubTimer,this);
+	if( -1 == m_tid_reg )
+		return -1;
+	m_tid_repeat = zloop_timer(loop,20,0,onRepeatPubTimer,this);
+	if( -1 == m_tid_repeat ) {
+		zloop_timer_end(m_loop,m_tid_reg);
+		m_tid_reg = -1;
+		return -1;
+	}
+	return 0;
+}
+
+void RIPublisher::stopLoop(zloop_t* loop) {
+	if( m_tid_reg != -1 ) {
+		zloop_timer_end(loop,m_tid_reg);
+		m_tid_reg = -1;
+	}
+	if( m_tid_repeat != -1 ) {
+		zloop_timer_end(loop,m_tid_repeat);
+		m_tid_repeat = -1;
+	}
+}
+
+void RIPublisher::onNewRegion(const Region& reg) {
+}
+
+void RIPublisher::onDelRegion(const uuid_t& reg) {
+}
+
+void RIPublisher::onNewService(const RegionRt& reg,const Service& svc) {
+	pubService(reg,svc);
+}
+
+void RIPublisher::onDelService(const RegionRt& reg,const uuid_t& svc) {
+	pubRemoveService(reg,svc);
+}
+
+void RIPublisher::onNewPayload(const RegionRt& reg,const Payload& pl) {
+	pubPayload(reg,pl);
+}
+
+void RIPublisher::onDelPayload(const RegionRt& reg,const uuid_t& pl) {
+	pubRemovePayload(reg,pl);
 }
 
 int RIPublisher::pubRegion(const RegionRt& region) {
@@ -58,7 +124,12 @@ int RIPublisher::pubRegion(const RegionRt& region) {
 
 int RIPublisher::pubRemoveRegion(const uuid_t& reg) {
 	zmsg_t* msg = RegionRt::toPublishDel(reg);
-	return zmsg_send(&msg,m_pub);
+	if( -1 != zmsg_send(&msg,m_pub) ) {
+		zsock_flush(m_pub);
+		return 0;
+	} else {
+		return -1;
+	}
 }
 
 int RIPublisher::pubService(const RegionRt& region,const Service& svc) {
@@ -82,3 +153,49 @@ int RIPublisher::pubRemovePayload(const RegionRt& region,const uuid_t& pl) {
 }
 
 
+zsock_t* RIPublisher::connectBrokers() {
+	zsock_t* sock = nullptr;
+	assert( ! m_brokers.empty() );
+	do {
+		sock = zsock_new(ZMQ_PUB);
+		if( nullptr == sock )
+			break;
+
+		for( auto it = m_brokers.begin(); it != m_brokers.end(); ++it) {
+			if( -1 == zsock_connect(sock,"%s",it->c_str()) ) {
+				zsock_destroy(&sock);
+				break;
+			}
+		}
+	} while( 0 );
+
+
+	return sock;
+}
+
+int RIPublisher::onRegionPubTimer(zloop_t *loop, int timer_id, void *arg) {
+	RIPublisher* self = (RIPublisher*)arg;
+	return self->pubRegion( self->m_table->region() );
+}
+
+int RIPublisher::onRepeatPubTimer(zloop_t *loop, int timer_id, void *arg) {
+	RIPublisher* self = (RIPublisher*)arg;
+	return self->pubRepeated();
+}
+
+
+int RIPublisher::pubRepeated() {
+	auto region = m_table->region();
+
+	auto svclist = m_table->update_timeouted_service(60000);
+	for(auto it=svclist.begin(); it != svclist.end(); ++it) {
+		pubService(region,*it);
+	}
+
+	auto pldlist = m_table->update_timeouted_payload(60000);
+	for(auto it=pldlist.begin(); it != pldlist.end(); ++it) {
+		pubPayload(region,*it);
+	}
+
+	return 0;
+}
