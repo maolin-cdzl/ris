2588b3747c2a6e95714d162ff94c018c1e379d20
毛林 <maolin@shanlitech.com>
2015-08-27 19:13:42 +0800

    update test_region

--- test/test_region.cxx
+++ test/test_region.cxx
@@ -9,12 +9,12 @@ int main(int argc,char* argv[]) {
 	void* s = region_open();
 
 	char id[32];
-	for(size_t i=1; i <= 1000; ++i) {
+	for(size_t i=1; i <= 50000; ++i) {
 		sprintf(id,"%lu",i);
 		region_new_payload(s,id);
 	}
 	region_close(s);
-	sleep(600);
+	sleep(300);
 	region_stop();
 	return 0;
 }
