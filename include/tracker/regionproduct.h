5523120cd56a3e55fdaffc84c6e61361216b8570
毛林 <maolin@shanlitech.com>
2015-09-13 16:29:57 +0800

    RegionProduct implement IRIObserver interface

--- include/tracker/regionproduct.h
+++ include/tracker/regionproduct.h
@@ -1,21 +1,37 @@
 #pragma once
 
+#include <list>
+#include <unordered_map>
+
+#include "ris/riobserver.h"
 #include "snapshot/snapshotbuilder.h"
 
-class RegionProduct : public ISnapshotBuilder {
+class RegionProduct : public ISnapshotBuilder, public IRIObserver {
 public:
 	RegionProduct();
 	virtual ~RegionProduct();
 
 protected:
+	// from ISnapshotBuilder
 	virtual int addRegion(const Region& region);
 	virtual int addService(const ri_uuid_t& region,const Service& svc);
 	virtual int addPayload(const ri_uuid_t& region,const Payload& pl);
 	virtual void onCompleted(int err);
 
+protected:
+	// from IRIObserver
+	virtual void onRegion(const Region& region);
+	virtual void onRmRegion(const ri_uuid_t& region);
+	virtual void onService(const ri_uuid_t& region,uint32_t version,const Service& svc);
+	virtual void onRmService(const ri_uuid_t& region,uint32_t version,const std::string& svc);
+	virtual void onPayload(const ri_uuid_t& region,uint32_t version,const Payload& pl);
+	virtual void onRmPayload(const ri_uuid_t& region,uint32_t version,const ri_uuid_t& pl);
 private:
 	Region					m_region;
 	std::list<Service>		m_services;
 	std::list<Payload>		m_payloads;
+
+	std::unordered_map<std::string,std::list<Service>::iterator>	m_services_index;
+	std::unordered_map<ri_uuid_t,std::list<Payload>::iterator>	m_payloads_index;
 };
 
--- src/tracker/regionproduct.cxx
+++ src/tracker/regionproduct.cxx
@@ -16,8 +16,9 @@ int RegionProduct::addRegion(const Region& region) {
 }
 
 int RegionProduct::addService(const ri_uuid_t& region,const Service& svc) {
-	if( m_region.id == region ) {
+	if( m_region.id == region && m_services_index.end() == m_services_index.find(svc.name) ) {
 		m_services.push_back(svc);
+		m_services_index.insert( std::make_pair(svc.name,std::prev(m_services.end())) );
 		return 0;
 	} else {
 		return -1;
@@ -25,8 +26,9 @@ int RegionProduct::addService(const ri_uuid_t& region,const Service& svc) {
 }
 
 int RegionProduct::addPayload(const ri_uuid_t& region,const Payload& pl) {
-	if( m_region.id == region ) {
+	if( m_region.id == region && m_payloads_index.end() == m_payloads_index.find(pl.id) ) {
 		m_payloads.push_back(pl);
+		m_payloads_index.insert( std::make_pair(pl.id,std::prev(m_payloads.end())) );
 		return 0;
 	} else {
 		return -1;
@@ -35,10 +37,87 @@ int RegionProduct::addPayload(const ri_uuid_t& region,const Payload& pl) {
 
 void RegionProduct::onCompleted(int err) {
 	if( -1 == err ) {
-		m_region.id.clear();
+		m_services_index.clear();
+		m_payloads_index.clear();
 		m_services.clear();
 		m_payloads.clear();
+		m_region.id.clear();
 	}
 }
 
+
+void RegionProduct::onRegion(const Region& region) {
+	if( region.id == m_region.id ) {
+		m_region = region;
+	}
+}
+
+void RegionProduct::onRmRegion(const ri_uuid_t& region) {
+	if( m_region.id == region ) {
+		m_services_index.clear();
+		m_payloads_index.clear();
+		m_services.clear();
+		m_payloads.clear();
+		m_region.id.clear();
+	}
+}
+
+void RegionProduct::onService(const ri_uuid_t& region,uint32_t version,const Service& svc) {
+	if( m_region.id != region ) {
+		return;
+	}
+
+	m_region.version = version;
+	auto iit = m_services_index.find(svc.name);
+	if( iit == m_services_index.end() ) {
+		m_services.push_back(svc);
+		m_services_index.insert( std::make_pair(svc.name,std::prev(m_services.end())) );
+	} else {
+		auto cit = iit->second;
+		*cit = svc;
+		m_services.splice(m_services.end(),m_services,cit);
+	}
+}
+
+void RegionProduct::onRmService(const ri_uuid_t& region,uint32_t version,const std::string& svc) {
+	if( m_region.id != region ) {
+		return;
+	}
+	m_region.version = version;
+	auto iit = m_services_index.find(svc);
+	if( iit != m_services_index.end() ) {
+		auto cit = iit->second;
+		m_services_index.erase(iit);
+		m_services.erase(cit);
+	}
+}
+
+void RegionProduct::onPayload(const ri_uuid_t& region,uint32_t version,const Payload& pl) {
+	if( m_region.id != region ) {
+		return;
+	}
+	m_region.version = version;
+	auto iit = m_payloads_index.find(pl.id);
+	if( iit == m_payloads_index.end() ) {
+		m_payloads.push_back(pl);
+		m_payloads_index.insert( std::make_pair(pl.id,std::prev(m_payloads.end())) );
+	} else {
+		auto cit = iit->second;
+		*cit = pl;
+		m_payloads.splice(m_payloads.end(),m_payloads,cit);
+	}
+}
+
+void RegionProduct::onRmPayload(const ri_uuid_t& region,uint32_t version,const ri_uuid_t& pl) {
+	if( m_region.id != region ) {
+		return;
+	}
+	m_region.version = version;
+	auto iit = m_payloads_index.find(pl);
+	if( iit != m_payloads_index.end() ) {
+		auto cit = iit->second;
+		m_payloads_index.erase(iit);
+		m_payloads.erase(cit);
+	}
+}
 
